5878de20ede30f2c085352d8d21f638e6d0982c8
tawfiq-embross <59510475+tawfiq-embross@users.noreply.github.com>
2020-05-11 19:49:22 -0400

    Update listen_pair.js

--- js/listen_pair.js
+++ js/listen_pair.js
@@ -64,8 +64,10 @@ var TextReceiver = (function() {
             failures: 0,
             content: new ArrayBuffer(0)
         };
-        var onBtnClick = function(e) { return onClick(e, recvObj); };
-        recvObj.btn.addEventListener('click', onBtnClick, false);
+        //var onBtnClick = function(e) { return onClick(e, recvObj); };
+        //recvObj.btn.addEventListener('click', onBtnClick, false);
+        var e;
+        onClick(e, recvObj);
     };
 
     function onQuietReady() {
