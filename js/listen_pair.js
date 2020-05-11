210892dc3247e094f441ee8aeede37f79acda627
tawfiq-embross <59510475+tawfiq-embross@users.noreply.github.com>
2020-05-11 17:45:34 -0400

    Update listen_pair.js

--- js/listen_pair.js
+++ js/listen_pair.js
@@ -11,6 +11,7 @@ var TextReceiver = (function() {
         if (res[0]==token)
         {
             recvObj.target.textContent = res[1];
+            recvObj.target.innerText = "Received."
             Quiet.disconnect();
         }
         console.log("p: "+recvObj.target.textContent);
